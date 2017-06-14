6ec8b0811f41aba1323fc37f956c495f55b926cd
marcioaffonso <marcio_affonso@hotmail.com>
2017-06-14 17:17:38 +0200

    MODIFY fix IE bug

--- src/js/components/clientMirror/ResultPane.js
+++ src/js/components/clientMirror/ResultPane.js
@@ -20,6 +20,9 @@ class ResultPane extends React.Component {
     if(!this.head || !this.props.sdkValue) { 
       return;
     }
+    if(this.body) {
+      this.removeBodyNodes(true);
+    }
     const scriptOT = document.createElement('script');
     scriptOT.setAttribute('src',
       `https://static.opentok.com/v${this.props.sdkValue}/js/opentok.min.js`
@@ -33,9 +36,11 @@ class ResultPane extends React.Component {
       this.body.removeChild(this.html);
       this.body.removeChild(this.style);
       this.body.removeChild(this.script);
+      this.removeBodyNodes(false);
     } else {
       this.body = this.frame.contentDocument.body;
     }
+
     this.html = document.createElement('div');
     this.html.innerHTML = this.props.result.html;
 
@@ -50,6 +55,21 @@ class ResultPane extends React.Component {
     this.body.appendChild(this.script);
   }
 
+  isTbPluginObject(node) {
+    return node.nodeName === 'OBJECT' &&
+      node.id && node.id.indexOf('tb_plugin') > -1;
+  }
+
+  removeBodyNodes(removeTbPlugin) {
+    const self = this;
+    const children = Array.prototype.slice.call(this.body.childNodes);
+    children.forEach(function(child) {
+      if(self.isTbPluginObject(child) === removeTbPlugin) {
+        self.body.removeChild(child);
+      }
+    });
+  }
+  
   render() {
     return (
       <div className={this.props.className}>
